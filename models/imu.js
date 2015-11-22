93a8e57cc703c9fd390c5f474558ab178388731c
Jay Butera <buterajay@gmail.com>
2015-11-22 00:23:18 -0600

    Finish views merge

--- models/imu.js
+++ models/imu.js
@@ -3,11 +3,7 @@ var Schema   = mongoose.Schema;
 
 var IMUSchema = new Schema ({
     // Timestamp
-<<<<<<< HEAD
     date  : Date,
-=======
-    time  : Date,
->>>>>>> ca6dbe3295999a817eeac031e860e429abc7d205
     // Gyroscope
     rot_x : Number,
     rot_y : Number,
